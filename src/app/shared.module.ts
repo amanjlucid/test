70b67fd2bf9426cc9288fa8e8b1887e62bd26525
unknown <amanj@lucidux.com>
2020-11-04 17:35:17 +0530

    event portal event types change two

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';  
 import { SimpleTextFilterComponent } from './kendo-component/simple-text-filter.component';
 import { DateFormatPipe } from './_pipes/date-format.pipe'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -6,8 +7,8 @@ import { DateRangeFilterComponent } from './kendo-component/date-range-filter.co
 import { GridModule } from '@progress/kendo-angular-grid';
 import { DatePickerModule } from '@progress/kendo-angular-dateinputs';
 import { MultiCheckFilterComponent } from './kendo-component/multicheck-filter.component';
-import { CommonModule } from '@angular/common';  
-import { BrowserModule } from '@angular/platform-browser';
+
+
 
 @NgModule({
   imports: [ FormsModule, ReactiveFormsModule, GridModule, DatePickerModule, CommonModule],
