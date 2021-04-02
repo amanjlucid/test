9ee10304d9e3d03ee5ea32d297a5ace2e5ed2b57
navratanp7737 <navratanp@unv7.comn>
2021-04-02 17:57:13 +0530

    Work Order Asset Details

--- src/app/worksorders/worksorders-asset-detail/worksorders-asset-detail.component.ts
+++ src/app/worksorders/worksorders-asset-detail/worksorders-asset-detail.component.ts
@@ -151,7 +151,13 @@ export class WorksordersAssetDetailComponent implements OnInit {
 
   }
 
-
+  numberOnly(event): boolean {
+      const charCode = (event.which) ? event.which : event.keyCode;
+      if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+          return false;
+      }
+      return true;
+  }
   WorkOrderAssetDetailPhases() {
 
 
