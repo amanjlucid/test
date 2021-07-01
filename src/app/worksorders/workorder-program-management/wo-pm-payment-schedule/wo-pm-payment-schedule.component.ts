c4a6e94f4819f8e30eea24439a58ab6079ae0720
unknown <amanj@lucidux.com>
2021-07-01 13:14:29 +0530

    merged with wopm branch 01072021

--- src/app/worksorders/workorder-program-management/wo-pm-payment-schedule/wo-pm-payment-schedule.component.ts
+++ src/app/worksorders/workorder-program-management/wo-pm-payment-schedule/wo-pm-payment-schedule.component.ts
@@ -170,7 +170,6 @@ export class WoProgramManagmentPaymentScheduleComponent implements OnInit {
 
   cellClickHandler({ dataItem }) {
     this.setSelectedRow(dataItem);
-    // this.selectedSingleDefect = dataItem;
   }
 
 
